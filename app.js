01cd4cb39213c82857e6f8b9a4694462839f68f1
Evette Cheung <FrancFranc@gmx.com>
2017-05-11 07:12:45 -0700

    build a constructor. draw the homepage roughly.

---
+++ app.js
@@ -0,0 +1,9 @@
+var content = [];
+
+  function Contents (rawDataObj) {
+
+ this.homepage = content.homepage;
+ this.about = content.about;
+ this.lab = content.lab;
+ this.contact = content.contact;
+ }

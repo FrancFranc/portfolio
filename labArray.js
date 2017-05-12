9ee638d731c32cfa6ff8c7bd5cf77300b8fcbfda
Evette Cheung <FrancFranc@gmx.com>
2017-05-11 22:02:13 -0700

    Tabs work fine.

--- labArray.js
+++ labArray.js
@@ -1,45 +1,27 @@
 'use strict';
 
-var labImagesArray = [
+var labData = [
   {
-    backgroundImage:  'img.url',
+    backgroundImage:  'https://www.elliecashmandesign.com/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/e/l/elliecashmanfloralwalllpaperdarkfloraldetail6.jpg',
     title:            'Bus Mall',
-    icon:             'img.url'
+    year:             '2017',
+    description:      'BusMall wants to do market analysis on proposed products to test their potential customer interest. Individuals chooses which product of three displayed images in 25 sets of selections, that they would be most likely to purchase.  Result data are stored, calculated, and visually displayed.  No result is shown to users until the end. Also, (i)total number of clicks, (ii)the percentage of times that an item was clicked when it was shown and (iii)how many times each image is displayed are calculated.',
+    labURL:           'https://github.com/FrancFranc/Bus-Mall'
   },
 
   {
-    backgroundImage:  'img.url',
+    backgroundImage:  'https://www.elliecashmandesign.com/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/e/l/elliecashmanfloralwalllpaperdarkfloraldetail6.jpg',
     title:            'Salmon Cookies Shop',
-    icon:             'img.url'
+    year:             '2017',
+    description:      'Store data are represented in a list format on the webpage, so my client can view the information.  A table to show the total amount of projected cookie needs at each location, with the table displaying the cookie stand location, the total number of cookies needed for each location, an hourly breakdown of total cookies sales for each location, and a row of totals for each column.',
+    labURL:           'https://github.com/FrancFranc/SalmonCookies'
   },
 
   {
-    backgroundImage:  'img.url',
+    backgroundImage:  'https://www.elliecashmandesign.com/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/e/l/elliecashmanfloralwalllpaperdarkfloraldetail6.jpg',
     title:            'Kilovolt Blog',
-    icon:             'img.url'
+    year:             '2017',
+    description:      'xxxxx.',
+    labURL:           'https://github.com/FrancFranc/SalmonCookies'
   }
 ]
-
-
-var labContentArray = [
-  {
-    title:        'Bus Mall',
-    year:         '2017',
-    description:  'BusMall wants to do market analysis on proposed products to test their potential customer interest. Individuals chooses which product of three displayed images in 25 sets of selections, that they would be most likely to purchase.  Result data are stored, calculated, and visually displayed.  No result is shown to users until the end. Also, (i)total number of clicks, (ii)the percentage of times that an item was clicked when it was shown and (iii)how many times each image is displayed are calculated.',
-    labURL:    'https://github.com/FrancFranc/Bus-Mall'
-  },
-
-  {
-    title:        'Salmon Cookies Shop',
-    year:         '2017',
-    description:  'Store data are represented in a list format on the webpage, so my client can view the information.  A table to show the total amount of projected cookie needs at each location, with the table displaying the cookie stand location, the total number of cookies needed for each location, an hourly breakdown of total cookies sales for each location, and a row of totals for each column.',
-    labURL:    'https://github.com/FrancFranc/SalmonCookies'
-  },
-
-  {
-    title:        'Kilovolt Blog',
-    year:         '2017',
-    description:  'xxxxx.',
-    labURL:    'https://github.com/FrancFranc/SalmonCookies'
-  }
-];
